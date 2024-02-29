35c5ffabefa5a2d0c85451da444ead008860a4dd
Rafael <dark.drago.200@gmail.com>
2024-02-29 18:53:49 +0300

    feat: added redis connection

---
+++ pkg/redis/redis.go
@@ -0,0 +1,26 @@
+package redis
+
+import (
+	"github.com/Snorkin/auth_service/config"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
+
+func CreateRedisClient(cfg *config.Config) *redis.Client {
+	addr := cfg.Redis.Address
+
+	if addr == "" {
+		addr = ":6379"
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr:         addr,
+		MinIdleConns: cfg.Redis.MinIdleConns,
+		PoolSize:     cfg.Redis.PoolSize,
+		PoolTimeout:  time.Duration(cfg.Redis.PoolTimeout) * time.Second,
+		Password:     cfg.Redis.Password,
+		DB:           cfg.Redis.DB, // 0 for default
+	})
+
+	return client
+}
