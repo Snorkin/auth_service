bf2b1bf742a0df804f7c051012639c9324627501
Rafael <dark.drago.200@gmail.com>
2024-02-29 03:27:52 +0300

    feat: user model added

---
+++ internal/models/user.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	UserID    uuid.UUID `json:"user_id" db:"user_id" validate:"omitempty"`
+	Email     string    `json:"email" db:"email" validate:"omitempty,lte=60,email"`
+	Password  string    `json:"password,omitempty" db:"password"`
+	CreatedAt time.Time `json:"created_at,omitempty" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at,omitempty" db:"updated_at"`
+}
