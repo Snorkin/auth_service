88a354b42aeb0d5cedff6c42c87b9e08ef422581
Rafael <dark.drago.200@gmail.com>
2024-02-29 23:12:17 +0300

    feat: logger and redis added

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"github.com/Snorkin/auth_service/pkg/logger"
+	cache "github.com/Snorkin/auth_service/pkg/redis"
+	"github.com/go-redis/redis/v8"
+	"github.com/jmoiron/sqlx"
 	"log"
 
 	"github.com/Snorkin/auth_service/config"
@@ -16,14 +20,29 @@ func main() {
 		log.Fatalln("Cannot load env variables")
 	}
 
+	appLogger := logger.CreateLogger(cfg)
+	appLogger.Init()
+	appLogger.Infof("Version: %s, Mode: %s, Loglevel: %s, SSL: %v", cfg.Server.AppVersion, cfg.Server.Mode, cfg.Logger.Level, cfg.Server.SSL)
+
 	pgDB, err := postgres.CreatePostgresDB(cfg)
 	if err != nil {
 		log.Fatalf("Cannot connect to pg database, %s\n", err)
 	}
+	defer func(pgDB *sqlx.DB) {
+		err := pgDB.Close()
+		if err != nil {
+			log.Fatalf("Error while closing db connection, %s", err)
+		}
+	}(pgDB)
 
-	server := authServer.CreateAuthApp(cfg, pgDB)
-	err = server.Run()
-	if err != nil {
-		log.Fatalf("Cannot run the app")
-	}
+	redisDB := cache.CreateRedisClient(cfg)
+	defer func(redisDB *redis.Client) {
+		err := redisDB.Close()
+		if err != nil {
+			log.Fatalf("Error while closing cache db connection, %s", err)
+		}
+	}(redisDB)
+
+	server := authServer.CreateAuthApp(appLogger, cfg, pgDB, redisDB)
+	appLogger.Fatal(server.Run())
 }
