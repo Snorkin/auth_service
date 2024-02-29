b548fe869bca8f96c40e2b399d98cd5a5b146deb
Rafael <dark.drago.200@gmail.com>
2024-02-29 20:10:23 +0300

    ref: added logger and unimplemented UserServiceServer for compatability

--- internal/user/delivery/grpc/service.go
+++ internal/user/delivery/grpc/service.go
@@ -3,14 +3,18 @@ package service
 import (
 	"github.com/Snorkin/auth_service/config"
 	"github.com/Snorkin/auth_service/internal/user"
+	"github.com/Snorkin/auth_service/pkg/logger"
+	userService "github.com/Snorkin/auth_service/proto"
 )
 
 type UsersService struct {
+	logger logger.Logger
 	cfg    *config.Config
 	userUC user.Useсase
+	userService.UnimplementedUserServiceServer
 	//sessUC session.SessionUsecase
 }
 
-func NewAuthServiceGRPC(cfg *config.Config, userUC user.Useсase) *UsersService {
-	return &UsersService{cfg: cfg, userUC: userUC}
+func NewAuthServiceGRPC(logger logger.Logger, cfg *config.Config, userUC user.Useсase) *UsersService {
+	return &UsersService{logger: logger, cfg: cfg, userUC: userUC}
 }
