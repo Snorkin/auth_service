1eebf2b8a1bfd681a85b02d4d04cfe0441840742
Rafael <dark.drago.200@gmail.com>
2024-02-29 21:59:58 +0300

    ref: get config new env parsed fields added

--- config/config.go
+++ config/config.go
@@ -17,7 +17,6 @@ type Config struct {
 type ServerConfig struct {
 	AppVersion  string
 	Port        string
-	PprofPort   string
 	Mode        string // Developer or Production TODO: add enum?
 	MaxConnIdle time.Duration
 	Timeout     time.Duration
@@ -31,7 +30,7 @@ type PostgresConfig struct {
 	User     string
 	Password string
 	DBName   string
-	SSLMode  bool
+	SSLMode  string
 	PgDriver string
 }
 
@@ -70,15 +69,38 @@ func GetConfig(path string) (*Config, error) {
 	if err != nil {
 		return nil, err
 	}
-
+	//TODO: check viper
 	cfg := Config{}
+
+	cfg.Server.AppVersion = os.Getenv("SERVER_VERSION")
+	cfg.Server.Port = os.Getenv("SERVER_PORT")
+	cfg.Server.Mode = os.Getenv("SERVER_MODE")
+	cfg.Server.MaxConnIdle, _ = time.ParseDuration(os.Getenv("SERVER_MAXCONNIDLE"))
+	cfg.Server.Timeout, _ = time.ParseDuration(os.Getenv("SERVER_TIMEOUT"))
+	cfg.Server.MaxConnAge, _ = time.ParseDuration(os.Getenv("SERVER_MAXCONNAGE"))
+	cfg.Server.Time, _ = time.ParseDuration(os.Getenv("SERVER_TIME"))
+
 	cfg.Postgres.Port = os.Getenv("DB_PORT")
 	cfg.Postgres.Host = os.Getenv("DB_HOST")
 	cfg.Postgres.User = os.Getenv("DB_USER")
 	cfg.Postgres.Password = os.Getenv("DB_PASSWORD")
 	cfg.Postgres.DBName = os.Getenv("DB_NAME")
-	cfg.Postgres.SSLMode, _ = strconv.ParseBool(os.Getenv("DB_SSL"))
+	cfg.Postgres.SSLMode = os.Getenv("DB_SSL")
 	cfg.Postgres.PgDriver = os.Getenv("DB_DRIVER")
+
+	cfg.Redis.DB, _ = strconv.Atoi(os.Getenv("REDIS_DB"))
+	cfg.Redis.Address = os.Getenv("REDIS_ADDRESS")
+	cfg.Redis.Password = os.Getenv("REDIS_PASSWORD")
+	cfg.Redis.PoolTimeout, _ = strconv.Atoi(os.Getenv("REDIS_POOLTIMEOUT"))
+	cfg.Redis.PoolSize, _ = strconv.Atoi(os.Getenv("REDIS_POOLSIZE"))
+	cfg.Redis.MinIdleConns, _ = strconv.Atoi(os.Getenv("REDIS_MINIDLECONN"))
+
+	check := false
+	if os.Getenv("SERVER_MODE") == "Development" {
+		check = true
+	}
+	cfg.Logger.Development = check
+	cfg.Logger.Encoding = os.Getenv("LOGGER_ENCODING")
 
 	return &cfg, nil
 }
