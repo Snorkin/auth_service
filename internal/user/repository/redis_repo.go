989c1f8d708ac45f2fa6a80f416890be4d505afa
Rafael <dark.drago.200@gmail.com>
2024-03-01 08:16:13 +0300

    feat: user redis repository added

---
+++ internal/user/repository/redis_repo.go
@@ -0,0 +1,56 @@
+package repository
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/Snorkin/auth_service/internal/models"
+	"github.com/Snorkin/auth_service/internal/user"
+	"github.com/Snorkin/auth_service/pkg/grpc_errors"
+	"github.com/Snorkin/auth_service/pkg/logger"
+	"github.com/Snorkin/auth_service/pkg/utils"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
+
+const (
+	prefix = "user:"
+)
+
+type userRedisRepo struct {
+	redisClient *redis.Client
+	logger      logger.Logger
+}
+
+func CreateUserRedisRepository(redisClient *redis.Client, logger logger.Logger) user.RedisRepository {
+	return &userRedisRepo{
+		redisClient: redisClient,
+		logger:      logger,
+	}
+}
+
+func (u *userRedisRepo) GetByIdCtx(ctx context.Context, key string) (*models.User, error) {
+	userBytes, err := u.redisClient.Get(ctx, utils.CreateKey(prefix, key)).Bytes()
+	if err != nil {
+		if err != redis.Nil {
+			return nil, grpc_errors.ErrNotFound
+		}
+		return nil, err
+	}
+	checkUser := &models.User{}
+	if err = json.Unmarshal(userBytes, checkUser); err != nil {
+		return nil, err
+	}
+	return checkUser, nil
+}
+
+func (u *userRedisRepo) SetUserCtx(ctx context.Context, key string, seconds int, user *models.User) error {
+	userBytes, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+	return u.redisClient.Set(ctx, utils.CreateKey(prefix, key), userBytes, time.Second*time.Duration(seconds)).Err()
+}
+
+func (u *userRedisRepo) DeleteUserCtx(ctx context.Context, key string) error {
+	return u.redisClient.Del(ctx, utils.CreateKey(prefix, key)).Err()
+}
