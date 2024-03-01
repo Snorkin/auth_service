f69c126ddfdb16d50a522e42cbc6cb5a3e5eff55
Rafael <dark.drago.200@gmail.com>
2024-03-01 08:12:40 +0300

    ref: hashing methods added

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,15 +1,47 @@
 package models
 
 import (
-	"time"
-
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"strings"
+	"time"
 )
+
+//TODO: maybe make dto for working with user w/o credentials instead of clearing password
 
 type User struct {
 	UserID    uuid.UUID `json:"user_id" db:"user_id" validate:"omitempty"`
+	Name      string    `json:"name,omitempty" db:"name"`
+	Surname   string    `json:"surname,omitempty" db:"surname"`
 	Email     string    `json:"email" db:"email" validate:"omitempty,lte=60,email"`
 	Password  string    `json:"password,omitempty" db:"password"`
 	CreatedAt time.Time `json:"created_at,omitempty" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at,omitempty" db:"updated_at"`
+}
+
+func (u *User) HashPassword() error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hash)
+	return nil
+}
+
+func (u *User) ComparePasswords(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+}
+
+func (u *User) PrepareCreate() error {
+	u.Email = strings.ToLower(strings.TrimSpace(u.Email))
+	u.Password = strings.TrimSpace(u.Password)
+
+	if err := u.HashPassword(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *User) ClearPassword() {
+	u.Password = ""
 }
